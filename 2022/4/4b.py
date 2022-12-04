da981e8991234bf3d2d8b63dcc7801c845ec36ba
LadyChristina <christina_ovezik@outlook.com>
2022-12-04 12:17:38 +0000

    Remove redundant condition

--- 2022/4/4b.py
+++ 2022/4/4b.py
@@ -5,7 +5,7 @@ with open(file) as f:
     for pair in f.read().splitlines():
         chores = [chores.split('-') for chores in pair.split(',')]
         chores1, chores2 = list(map(lambda x: range(int(x[0]), int(x[1])+1), chores))
-        if any(chore in chores2 for chore in chores1) or all(chore in chores1 for chore in chores2):
+        if any(chore in chores2 for chore in chores1):
             overlapping_chores += 1 
             
 print(f'Solution: {overlapping_chores}')
